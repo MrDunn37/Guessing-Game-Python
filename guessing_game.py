fa0527a074a9c5eeac4178b5b6f1909b4e414fd8
MrDunn37 <PrestonDunn37@gmail.com>
2021-06-16 19:38:07 -0400

    Add files via upload

--- guessing_game.py
+++ guessing_game.py
@@ -35,31 +35,28 @@ def start_game():
     name = input("What do they call you friend? ")
     answer = random.randint(1,10)
     total_guesses = 1
-    try:
-      guess = int(input("Pick a number between 1 and 10 {} ".format(name)))
-    except ValueError as err:
-      print("So {}, I assume this is your first time playing a guessing game.  I neeed a WHOLE NUMBER ONLY BETWEEN 1 AND 10 MY FRIEND!".format(name))
-    else:
-      while guess != answer:
+    while True:
+       try:
+          guess = int(input("Pick a number between 1 and 10 {} ".format(name)))
+       except ValueError as err:
+        print("So {}, I assume this is your first time playing a guessing game.  I neeed a WHOLE NUMBER ONLY BETWEEN 1 AND 10 MY FRIEND!".format(name))
+       else:
         if guess > 10:
           print("Sorry {}, you guess cannot be larger than 10".format(name))
-          guess = int(input("Pick a number between 1 and 10 {} ".format(name)))
-        elif guess <0:
+        elif guess < 1:
           print("Sorry {}, you guess cannot be smaller than 0".format(name))
-          guess = int(input("Pick a number between 1 and 10 {} ".format(name)))
         elif guess > answer:
           print("{}, it's lower".format(name))
-          guess = int(input("Pick a number between 1 and 10 {} ".format(name)))
           total_guesses += 1
         elif guess < answer:
           print("{}, it's higher".format(name))
-          guess = int(input("Pick a number between 1 and 10 {} ".format(name)))
           total_guesses += 1
         else:
           if guess == answer:
             break
-      print(("Good Job {},you got it! The correct number was ".format(name) + str(answer) +  " and it took you a total of "+ str(total_guesses) + " guesses"))
-      print("Thanks for coming to play today, next time your feeling lucky come back and pay me another visit!")
+              
+    print(("Good Job {},you got it! The correct number was ".format(name) + str(answer) +  " and it took you a total of "+ str(total_guesses) + " guesses"))      
+    print("Thanks for coming to play today, next time your feeling lucky come back and pay me another visit!")
     
 # Kick off the program by calling the start_game function.
 start_game()
